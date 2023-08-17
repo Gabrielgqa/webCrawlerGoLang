0bf9e040707a436dbdd2ecb52822540c00202023
Gabrielgqa <gabrielgqa@gmail.com>
2023-08-17 16:49:10 +0000

    feat: extract all links

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module main
 
 go 1.19
+
+require golang.org/x/net v0.14.0
--- main.go
+++ main.go
@@ -2,8 +2,61 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"net/url"
+
+	"golang.org/x/net/html"
 )
 
+var links []string
+
 func main() {
-	fmt.Println("Hello, World!")
+	visitUrl("https://github.com/Gabrielgqa")
+
+	fmt.Println(len(links))
+}
+
+func visitUrl(url string) {
+	fmt.Println(url)
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Sprintf("status diferente de 200: %d", resp.StatusCode))
+	}
+
+	doc, err := html.Parse(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	extractLinksFromUrl(doc)
+}
+
+func extractLinksFromUrl(element *html.Node) {
+	if element.Type == html.ElementNode && element.Data == "a" {
+		for _, attr := range element.Attr {
+			if attr.Key != "href" {
+				continue
+			}
+
+			link, err := url.Parse(attr.Val)
+			if err != nil || link.Scheme == "" {
+				continue
+			}
+
+			links = append(links, link.String())
+
+			visitUrl(link.String())
+		}
+	}
+
+	for c := element.FirstChild; c != nil; c = c.NextSibling {
+		extractLinksFromUrl(c)
+	}
 }
